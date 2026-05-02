bc87c743b941e42b58e3670dee4a9f05088a748e
BuckBeatable <unbeatable1687@gmail.com>
2026-05-03 01:11:00 +0900

    netilfy 빌드 오류 수정(최신 브라우저 기준 빌드 설정 추가)

--- vite.config.ts
+++ vite.config.ts
@@ -19,25 +19,11 @@ export default defineConfig({
     port: 3000, // 원하시는 포트 번호로 변경
   },
   worker: {
-    // Vite에게 .ts 파일을 워커 스크립트로 처리하도록 명시적으로 알립니다.
-    // 'module' 타입 워커에 대한 지원을 강화합니다.
     plugins: () => [react()], // 워커에도 React 플러그인을 적용하여 TSX 등을 처리할 수 있게 함 (선택적)
     format: 'es'
   },
   build: {
-    // rollupOptions: {
-    //   output: {
-    //     // Wasm 파일의 경로를 정확하게 설정합니다.
-    //     // 이 부분은 기존에 이미 있을 수 있으며, WASM 파일이 assets 폴더에 제대로 있는지 확인해야 합니다.
-    //     assetFileNames: (assetInfo) => {
-    //       // 예를 들어, wasm_image_cropper_bg.wasm이 직접 assets 폴더에 복사되는 경우
-    //       if (assetInfo.name && assetInfo.name.includes('wasm_image_cropper_bg.wasm')) {
-    //         return `assets/[name][extname]`; // 해시 없이 원본 이름 유지
-    //       }
-    //       return 'assets/[name]-[hash][extname]'; // 그 외 파일은 해시 적용
-    //     },
-    //   },
-    // },
+    target: 'esnext', //최신 브라우저를 기준으로 빌드
   },
   optimizeDeps: {
     // 워커 스크립트를 Vite가 제대로 처리하도록 명시적으로 추가합니다.
